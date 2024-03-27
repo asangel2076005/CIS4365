a33e761b960200fb0ba01f33e085e340823efd78
asangel2076005 <juuzousuzuya200@gmail.com>
2024-03-27 03:24:04 -0500

    Update q11.sql

--- SQL_Problems/q11.sql
+++ SQL_Problems/q11.sql
@@ -4,14 +4,17 @@ Query: Compare the actual expenses against the budget for each department for th
 Entities Involved: EXPENSE, BUDGET, DEPARTMENT 
 */
 
-SELECT *
+SELECT DEPT_NAME, (ALLOCATED_AMT - SUM(EXPENSE_AMT)) AS "Difference between budget and actual expenses"
 FROM EXPENSE E
 JOIN DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
 JOIN BUDGET B ON D.DEPT_ID = B.DEPT_ID
-ORDER BY DEPT_NAME;
+GROUP BY DEPT_NAME
+ORDER BY (ALLOCATED_AMT - SUM(EXPENSE_AMT)) DESC;
 
-SELECT SUM(EXPENSE_AMT)
-FROM EXPENSE E
-JOIN DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
-JOIN BUDGET B ON D.DEPT_ID = B.DEPT_ID
-WHERE DEPT_NAME = "Bakery";
+/*
+Joined three tables based upon their linking attribute: DEPT_ID
+In the ERD, the relationship goes from left to right starting with EXPENSE, to DEPARTMENT, to BUDGET
+So it seemed logical to begin with EXPENSE first despite being the many side of DEPARTMENT.
+Used aggregate functions to determine the difference between the budget amt and the sum of all incurred expenses by each department
+Also ordered the result by this aggregate functions and then grouped them by DEPT_NAME to avoid duplicates
+*/
