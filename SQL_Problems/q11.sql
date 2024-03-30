95e278984e2ee5d27bfc85dc30aec921674fe624
asangel2076005 <juuzousuzuya200@gmail.com>
2024-03-30 17:59:13 -0500

    Update q11.sql

--- SQL_Problems/q11.sql
+++ SQL_Problems/q11.sql
@@ -5,19 +5,31 @@ Entities Involved: EXPENSE, BUDGET, DEPARTMENT
 Angelo Angel
 */
 
-SELECT DEPT_NAME, (ALLOCATED_AMT - SUM(EXPENSE_AMT)) AS "Difference between budget and actual expenses"
-FROM EXPENSE E
-JOIN DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
-JOIN BUDGET B ON D.DEPT_ID = B.DEPT_ID
-WHERE FISCAL_YEAR = 2024
-GROUP BY DEPT_NAME
-ORDER BY (ALLOCATED_AMT - SUM(EXPENSE_AMT)) DESC;
+SELECT DEPARTMENT_BUDGET.DEPT_NAME, TOTAL_BUDGET - TOTAL_EXPENSE AS "Difference between budget and expenses"
+FROM (
+		SELECT D.DEPT_ID, DEPT_NAME, SUM(ALLOCATED_AMT) as TOTAL_BUDGET
+		FROM BUDGET B
+		JOIN DEPARTMENT D ON B.DEPT_ID = D.DEPT_ID
+		GROUP BY DEPT_NAME
+		ORDER BY DEPT_NAME
+        ) AS DEPARTMENT_BUDGET
+JOIN (
+		SELECT D.DEPT_ID, DEPT_NAME, SUM(EXPENSE_AMT) AS TOTAL_EXPENSE 
+		FROM EXPENSE E
+		JOIN DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
+		JOIN BUDGET B ON D.DEPT_ID = B.DEPT_ID
+		GROUP BY DEPT_NAME
+		ORDER BY DEPT_NAME
+        ) AS DEPARTMENT_EXPENSE
+        ON DEPARTMENT_EXPENSE.DEPT_ID = DEPARTMENT_BUDGET.DEPT_ID
+ORDER BY TOTAL_BUDGET - TOTAL_EXPENSE DESC;
 
 /*
-Joined three tables based upon their linking attribute: DEPT_ID
-In the ERD, the relationship goes from left to right starting with EXPENSE, to DEPARTMENT, to BUDGET
-So it seemed logical to begin with EXPENSE first despite being the many side of DEPARTMENT.
-Only chose entries tied to the year 2024
-Used aggregate functions to determine the difference between the budget amt and the sum of all incurred expenses by each department
-Also ordered the result by this aggregate functions and then grouped them by DEPT_NAME to avoid duplicates
+Joined two virtual tables using subquery where the results of each table become a new temporary table
+First table: Department Budget
+Found the sum of all budgets for each department joining budget and department tables.
+Second table: Department Expense
+Found the sum of all expenses for each department joining all three tables.
+Then I found the difference between budget and expenses leading to the result
+I ordered the difference in a descending order
 */
