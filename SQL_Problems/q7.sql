02cd0ecb48dbae2193ea7262c6777d395a640e43
MaybellineM <143357679+MaybellineM@users.noreply.github.com>
2024-04-03 20:40:10 -0500

    Add files via upload

---
+++ SQL_Problems/q7.sql
@@ -0,0 +1,27 @@
+/*
+Query #7
+Maybelline Medrano
+Catering Revenue
+Query: Summarize the total revenue generated from catering services for each event type.  
+Entities Involved: CATERING, EVENT 
+*/
+
+SELECT 
+    EVENT.EVENT_TYPE,
+    SUM(CATERING.CATER_REVENUE) AS TOTAL_REVENUE
+FROM 
+    EVENT
+JOIN 
+    CATERING ON EVENT.EVENT_CODE = CATERING.EVENT_CODE
+GROUP BY 
+    EVENT.EVENT_TYPE;
+    
+    
+/*
+Summary:
+- Joined the Catering and Event tables to calculate revenue for each event type
+- Calculated total revenue for each event type by summing up catering revenue
+- Grouped by Event Type
+*/  
+
+SELECT * FROM CATERING;
