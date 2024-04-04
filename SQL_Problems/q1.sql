69656e4ef1262c124c5f3fb0e0aac1caf8f0973e
Eman Arshad <143240493+emanarshad25@users.noreply.github.com>
2024-04-03 22:30:36 -0500

    Update q1.sql

--- SQL_Problems/q1.sql
+++ SQL_Problems/q1.sql
@@ -17,3 +17,7 @@ FROM CUSTOMER JOIN (SELECT CUST_ID
    
 					
 -- Joined the Customer and Order table to find all the orders and purchases made. 
+-- Grouped by Customer ID and used the HAVING clause to filter customers who have made more than 5 orders. 
+-- Left Join is used to inlcude all values from the Loyalty Program on the customer id where the loyalty program is null.
+-- If a customer who has made more than 5 purchases and is not enrolled in the loyalty program they will be shown. 
+
